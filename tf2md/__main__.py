f6e4dde4f75dad1954320bd3edd7a3c2a38e891b
Karl Webster <19327709+ktasper@users.noreply.github.com>
2023-01-27 21:02:56 +0000

    Init commit, it works

---
+++ tf2md/__main__.py
@@ -0,0 +1,27 @@
+import click
+from tf2md.markdown import create_output_file, create_variables_file
+from tf2md.parser import open_file, parse_hcl
+
+@click.group()
+@click.version_option("1.0.0")
+def main():
+  """Terraform to markdown"""
+  pass
+
+@main.command()
+@click.option(
+    "--file-type",
+    type=click.Choice(["variable", "output"]),
+    help="The type of tf file you wish to use",
+)
+@click.argument("input_file", type=click.Path(), required=True)
+def gen_docs(file_type, input_file):
+    """
+    This generates markdown docs for either variables.tf or outputs.tf
+    """
+    hcl_dict = open_file(input_file)
+    parsed_dict = parse_hcl(hcl_dict, file_type)
+    if file_type == "variable":
+        create_variables_file(parsed_dict)
+    else:
+        create_output_file(parsed_dict)
