447a6d300706e742a63fd22a464f979080b25b61
Karl Webster <19327709+ktasper@users.noreply.github.com>
2023-02-04 19:44:33 +0000

    Added markdown tests

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -1,8 +1,5 @@
 import os
 import sys
-import pytest
-from unittest.mock import MagicMock, call
-from mdutils.mdutils import MdUtils
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from tf2md.markdown import create_output_file, tf_var_type_cleaner, create_variables_file
@@ -41,3 +38,31 @@ def test_tf_var_type_cleaner():
     input_string = "${string} with ${extra} characters"
     expected_output = "string with extra characters"
     assert tf_var_type_cleaner(input_string) == expected_output
+
+
+
+
+def test_create_variables_file():
+    variables = [
+        {"name": "variable_1", "description": "Description of variable_1", "type": "string", "nullable": "yes", "default": "default_1"},
+        {"name": "variable_2", "description": "Description of variable_2", "type": "int", "nullable": "no", "default": None},
+        {"name": "variable_3", "description": "Description of variable_3", "type": "bool", "nullable": "yes", "default": "true"}
+    ]
+    variable_file = "TestVariables"
+    create_variables_file(variables, variable_file)
+    assert os.path.exists(f"{variable_file}.md")
+
+    # Clean up created file
+    os.remove(f"{variable_file}.md")
+
+def test_create_variables_file_default_variable_file_name():
+    variables = [
+        {"name": "variable_1", "description": "Description of variable_1", "type": "string", "nullable": "yes", "default": "default_1"},
+        {"name": "variable_2", "description": "Description of variable_2", "type": "int", "nullable": "no", "default": None},
+        {"name": "variable_3", "description": "Description of variable_3", "type": "bool", "nullable": "yes", "default": "true"}
+    ]
+    create_variables_file(variables)
+    assert os.path.exists("Variables.md")
+
+    # Clean up created file
+    os.remove("Variables.md")
